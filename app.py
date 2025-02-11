6efe5e0dafa16d6fe9e489df4ff11c512bbbc865
paola <l21590301@sjuanrio.tecnm.mx>
2025-02-11 16:45:44 -0600

    se agrega gestion de alumnos

--- app.py
+++ app.py
@@ -1,17 +1,47 @@
-from flask import Flask
+from flask import Flask, request, jsonify, render_template, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 #crear instancia
-app= Flask(__name__)
+app =  Flask(__name__)
 
-#ruta raíz
+# Configuración de la base de datos PostgreSQL
+app.config['SQLALCHEMY_DATABASE_URI'] = 'URLEXTERNA les d'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+#Modelo de la base de datos
+class Alumno(db.Model):
+    __tablename__ = 'alumnos'
+    no_control = db.Column(db.String, primary_key=True)
+    nombre = db.Column(db.String)
+    ap_paterno = db.Column(db.String)
+    ap_materno = db.Column(db.String)
+    semestre = db.Column(db.Integer)
+
+    def to_dict(self):
+        return{
+            'no_control': self.no_control,
+            'nombre': self.nombre,
+            'ap_paterno': self.ap_paterno,
+            'ap_materno': self.ap_materno,
+            'semestre': self.semestre,
+        }
+
+
+#Ruta raiz
 @app.route('/')
-def hola_mundo():
-    return 'Hola Mundo'
+def index():
+    #retornar los alumnos
 
-#ruta raíz
+    #return 'Hola Mundo'
+    return render_template('index.html')
+
+#Ruta /alumnos
 @app.route('/alumnos')
 def getAlumnos():
-    return 'Aquí van los alumnos'
+    return 'Aqui van los alumnos'
 
-if __name__== '__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
