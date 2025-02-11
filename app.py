1bc3573eeca9036e99110a0b62d8194c24e15f5c
paola <l21590301@sjuanrio.tecnm.mx>
2025-02-11 15:47:39 -0600

    se agrega ruta alumnos

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ app= Flask(__name__)
 def hola_mundo():
     return 'Hola Mundo'
 
+#ruta raíz
+@app.route('/alumnos')
+def getAlumnos():
+    return 'Aquí van los alumnos'
+
 if __name__== '__main__':
     app.run(debug=True)
