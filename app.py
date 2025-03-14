fb203da20b67e8a5aff6b791791b88407eea9fed
paola <l21590301@sjuanrio.tecnm.mx>
2025-03-13 18:10:57 -0600

    agregando variables de entorno

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
+import os
 from flask import Flask, request, jsonify, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
+
+from dotenv import load_dotenv
+
+#Cargar las variables de entorno
+load_dotenv()
 
 #crear instancia
 app =  Flask(__name__)
 
 # Configuración de la base de datos PostgreSQL
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://paogg:PWRrF1wBiAsOiqT1stZREB0tkZ79NbjD@dpg-cuiikclumphs7381jdlg-a.oregon-postgres.render.com/db_cetech'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -76,6 +82,44 @@ def update_alumno(no_control):
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('update_alumno.html', alumno=alumno)
+
+#Endpoint para obtener un estudiante por no_control
+@app.route('/alumnos/<no_control>', methods=['GET'])
+def obtener_alumno(no_control):
+    alumno = Alumno.query.get(no_control)
+    if alumno is None:
+        return jsonify({'mensaje': 'Alumno no encontrado'}), 404
+    return jsonify({
+        'no_control': alumno.no_control,
+        'nombre': alumno.nombre,
+        'ap_paterno': alumno.ap_paterno,
+        'ap_materno': alumno.ap_materno,
+        'semestre': alumno.semestre
+        })
+#Endpoint para actualizar un estudiante 
+@app.route('/alumnos/<no_control>', methods=['PUT'])
+def actualizar_alumno(no_control):
+    alumno = Alumno.query.get(no_control)
+    if alumno is None:
+        return jsonify({'mensaje': 'Alumno no encontrado'}), 404
+    data = request.get_json()
+    alumno.nombre = data['nombre']
+    alumno.ap_paterno = data['ap_paterno']
+    alumno.ap_materno = data['ap_materno']
+    alumno.semestre = data['semestre']
+    db.session.commit()
+    return jsonify({'mensaje': 'Alumno actualizado exitosamente'})
+
+#Endpoint para eliminar un estudiante
+@app.route('/alumnos/<no_control>', methods=['DELETE'])
+def eliminar_alumno(no_control):
+    alumno = Alumno.query.get(no_control)
+    if alumno is None:
+        return jsonify({'mensaje': 'Alumno no encontrado'}), 404
+    db.session.delete(alumno)
+    db.session.commit()
+    return jsonify({'mensaje': 'Alumno eliminado exitosamente'})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
